9dea1ea3ff47d26fe736af46ef59a205abc59593
Sangram-tech <sangrammahakud1@gmail.com>
2024-11-30 22:05:55 +0530

    changes

---
+++ C/sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Function declaration
+int add(int a, int b);
+
+int main() {
+    int num1, num2, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    sum = add(num1, num2);
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
+int add(int a, int b) {
+    return a + b;
+}
